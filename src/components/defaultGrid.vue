a23d74e683618cb1f027b991f769714b66c58445
cslee 08 <cslee08@cslee0182.onmicrosoft.com>
2020-03-10 17:20:22 +0900

    Checkbox, Combobox

--- src/components/defaultGrid.vue
+++ src/components/defaultGrid.vue
@@ -3,12 +3,11 @@
         <div id = 'cellTest' style="textAlign: left">
             Column : {{ this.selectedColumn }}<br/>
             Row : {{ this.selectedRow }}<br/>
-            <button v-on:click="removeSelectedRows()">열 삭제</button>
             <button v-on:click="setColumnVisible(false)">컬럼 삭제</button>
+            <button v-on:click="dataExtract()">데이터 확인</button>
         </div>
         <ag-grid-vue style="width: 100%; height: 50%;"
             class="ag-theme-balham"
-            :defaultColDef="defaultColDef"
             @grid-ready="onGridReady"
             :gridOptions="gridOptions"
             :columnDefs="columnDefs"
@@ -18,8 +17,7 @@
             :suppressRowClickSelection="true"
             :rowMultiSelectWithClick="true"
             @cell-focused="cellFocused()"
-            @cell-clicked="cellClicked()"
-            
+            @cell-mouse-out="cellMouseOut()"
             :enterMovesDownAfterEdit="true"
             :enterMovesDown="true">
         </ag-grid-vue>
@@ -45,91 +43,65 @@
                 columnDefs: null,
                 rowData: null,
                 gridApi: null,
-                defaultColDef: null,
                 selectedColumn: null,
                 selectedRow: null,
                 selectedData: [],
             }
         },
+
         components: {
             AgGridVue
         },
+
         methods:{
+            dataExtract(){
+                this.selectedData = []; // 데이터 배열 초기화
+
+                for(var i = 0; i < this.gridApi.getLastDisplayedRow(); i++){ // 보여지는 로우의 끝까지 검사
+                    var row = this.gridApi.getDisplayedRowAtIndex(i);
+
+                    if(document.getElementById("X"+i).checked == true && document.getElementById("Y"+i).checked == true){
+                        row.data.X = 1;
+                        row.data.Y = 1;
+                    } // 둘다 체크 되어 있으면 x : 1 / y : 1
+                    else if(document.getElementById("X"+i).checked == true){
+                        row.data.X = 1;
+                        row.data.Y = 0;
+                    }
+                    else if(document.getElementById("Y"+i).checked == true){
+                        row.data.X = 0;
+                        row.data.Y = 1;
+                    }
+                    else{
+                        row.data.X = 0;
+                        row.data.Y = 0;
+                        continue;
+                    } // 체크 안 되어 있으면 0, 0 및 데이터 배열에 추가 안함
+
+                    row.data.colType = document.getElementById("colType" + i).value
+                    this.selectedData.push(row.data); // 데이터 배열에 추가
+                }
+            },
+            
             cellFocused(){
                 var rowIndex = this.gridApi.getFocusedCell().rowIndex
 
                 var row = this.gridApi.getDisplayedRowAtIndex(rowIndex);
 
-                if(row.selected == true){
-                    row.setSelected(false);
-                    row.data.X = 0;
-                    row.data.Y = 0;
-                }
-                
-                else{
-                    if(this.gridApi.getFocusedCell().column.colId === 'X'){
-                        var selectedRows = this.gridApi.getSelectedNodes();
-                        selectedRows.forEach(element => {
-                            if(element.data.colType != 'int'){
-                                element.setSelected(false);
-                                element.data.X = 0;
-                                element.data.Y = 0;
-                            }
-                        });
-                    }
+                row.setSelected(true);
+            }, // 시각적 효과를 위해 cell에 포커스 되면 row가 선택되게 (흰색 -> 파란색)
 
-                    row.setSelected(true);
-                }
-
-                if(this.gridApi.getFocusedCell().column.colId === 'Y'){
-                    row.setDataValue('count',Number(row.data.count) + (rowIndex + 1))
-                }
-
-                this.selectedColumn = this.gridApi.getFocusedCell().column.colId;
-                this.selectedRow = this.gridApi.getFocusedCell().rowIndex;
-                var selectedRows = this.gridApi.getSelectedNodes();
-
-                this.selectedData=[];
-                var temp = []
-                selectedRows.forEach(element => {
-                    if(element.data.colType == "cha" || element.data.colType == "fac"){
-                        element.data.X = 1;
-                    }
-
-                    else if(element.data.colType == "int"){
-                        element.data.Y = 1;
-                    }
-
-                    else{
-                        element.data.X = 1;
-                        element.data.Y = 1;
-                    }
-
-                    temp.push(element.data)
-
-                    this.selectedData.push(temp)
-
-                    temp = [];
-
-                    this.gridApi.refreshCells();
-                })
-            },
-
-            cellClikced(){
+            cellMouseOut(){
                 var rowIndex = this.gridApi.getFocusedCell().rowIndex
 
                 var row = this.gridApi.getDisplayedRowAtIndex(rowIndex);
-            },
 
-            removeSelectedRows(){
-                var selectedRows = this.gridApi.getSelectedRows();
-
-                this.gridApi.updateRowData({remove:selectedRows})
-            },
+                row.setSelected(false);
+            }, // 마우스를 cell에서 빼면 선택 해제 (파란색 -> 흰색)
 
             setColumnVisible(params){
                 this.gridColumnApi.setColumnVisible(this.gridApi.getFocusedCell().column.colId,params)
-            },
+            }, // 컬럼 삭제
             
             onGridReady(params) {
                 params.api.sizeColumnsToFit();
@@ -137,66 +109,76 @@
         },
         beforeMount() {
             this.gridOptions = {};
-
-            /*this.columnDefs = [
-                {headerName: 'Name', field: 'name'},
-                {headerName: 'Age', field: 'age',cellStyle: { backgroundColor: "#aaffaa" }, checkboxSelection : function(params){
-                    if(params.data.age > "25")
-                        return true
-                }},
-                {headerName: 'Birthday', field: 'birthday',checkboxSelection : function(params){
-                    if(params.data.age <= "25")
-                        return true
-                }},
-                {headerName: 'Gender', field: 'gender',cellEditor: "agSelectCellEditor",
-                    cellEditorParams: {cellHeight: 50, values: ["male", "female"]}},
-                {headerName: 'Hobby', field: 'hobby'}
-            ];
-
-            this.rowData = [
-                {name: 'Sam', age: '30', birthday: '00/00/00', gender: 'male', hobby:'playing piano'},
-                {name: 'Ariana', age: '25', birthday: '00/00/00', gender: 'female', hobby:'soccer'},
-                {name: 'Elsa', age: '20', birthday: '00/00/00', gender: 'female', hobby:'watching youtube'},
-                {name: 'Steve', age: '24', birthday: '00/00/00', gender: 'male', hobby:'netflix'},
-                {name: 'Tom', age: '34', birthday: '00/00/00', gender: 'male', hobby:'cooking'}
-            ];*/
-
             this.columnDefs = [
                 {headerName: '컬럼명', field: 'colName'},
-                {headerName: '자료형식', field: 'colType'},
-                {headerName: 'X', field: 'X', checkboxSelection: function (params) {
-                    if(params.data.colType == "cha" || params.data.colType == "fac")
-                        return true
+                {headerName: '자료형식', field: 'colType', cellRenderer: function(params){
+                    var select = document.createElement("select") // select 생성
+
+                    var dataArray = ["int","cha","fac"] // 실제 데이터
+                    var textArray = ["정수","문자","요소"]  // 보여지는 값
+
+                    for(var i = 0 ; i < dataArray.length; i++){ // option 반복 생성 및 할당
+                        var option = document.createElement("option")
+
+                        option.value = dataArray[i] // 실제 데이터 할당
+                        option.text = textArray[i] // 보여지는 값 할당
+
+                        if(params.node.data.colType == dataArray[i]){
+                            option.defaultSelected=true; // 초기값 설정
+                        }
+
+                        select.appendChild(option)
+                    }
+
+                    select.id = "colType" + params.rowIndex;
+
+                    return select
                 }},
-                {headerName: 'Y', field: 'Y', checkboxSelection: function (params) {
-                    if(params.data.colType == "int")
-                        return true
+                {headerName: 'X', field: 'X', cellRenderer: function(params) {
+                    var input = document.createElement("input")
+                    input.type = "checkbox"
+                    input.checked = false;
+                    input.id = "X" + params.rowIndex; // 로우 index에 맞추어 id를 설정
+
+                    input.addEventListener('click', function (event) { // 클릭 이벤트 리스너
+
+                        for(var i = 0; i < params.node.parent.allChildrenCount; i++){ // 부모 노드가 가지고 있는 자식 노드의 갯수 가져와서 모든 노드 검사
+                            if(params.rowIndex == i){
+                                continue;
+                            }
+                            if(document.getElementById("X"+i).checked == true){
+                                document.getElementById("X"+i).checked = false
+                            }
+                        }
+                    });
+
+                    return input
                 }},
-                {headerName: 'Count', field: 'count'},
+                {headerName: 'Y', field: 'Y',cellRenderer: function(params) {
+                    var input = document.createElement("input")
+                    input.type = "checkbox"
+                    input.checked = false;
+                    input.id = "Y" + params.rowIndex;
+
+                    return input
+                }}
             ];
 
             this.rowData = [
-                {colName:'VI', colType:'int', X:'0', Y:'0', count:0},
-                {colName:'CO', colType:'int', X:'0', Y:'0', count:0},
-                {colName:'AB', colType:'fac', X:'0', Y:'0', count:0},
-                {colName:'FA', colType:'int', X:'0', Y:'0', count:0},
-                {colName:'DI', colType:'fac', X:'0', Y:'0', count:0},
-                {colName:'FN', colType:'cha', X:'0', Y:'0', count:0},
-                {colName:'FS', colType:'cha', X:'0', Y:'0', count:0},
-                {colName:'FC', colType:'cha', X:'0', Y:'0', count:0},
-                {colName:'FZ', colType:'int', X:'0', Y:'0', count:0},
-                {colName:'FS', colType:'int', X:'0', Y:'0', count:0},
-                {colName:'CO', colType:'fac', X:'0', Y:'0', count:0},
-                {colName:'DI', colType:'fac', X:'0', Y:'0', count:0},
-                {colName:'CH', colType:'cha', X:'0', Y:'0', count:0}
+                {colName:'VI', colType:'int', X:'0', Y:'0'},
+                {colName:'CO', colType:'int', X:'0', Y:'0'},
+                {colName:'AB', colType:'fac', X:'0', Y:'0'},
+                {colName:'FA', colType:'int', X:'0', Y:'0'},
+                {colName:'DI', colType:'fac', X:'0', Y:'0'},
+                {colName:'FN', colType:'cha', X:'0', Y:'0'},
+                {colName:'FS', colType:'cha', X:'0', Y:'0'},
+                {colName:'FC', colType:'cha', X:'0', Y:'0'},
+                {colName:'FZ', colType:'int', X:'0', Y:'0'},
+                {colName:'FS', colType:'int', X:'0', Y:'0'},
+                {colName:'CO', colType:'fac', X:'0', Y:'0'},
+                {colName:'DI', colType:'fac', X:'0', Y:'0'},
+                {colName:'CH', colType:'cha', X:'0', Y:'0'}
             ];
-
-            this.defaultColDef = {
-                editable: true,
-                resizable: true,
-                cellStyle:{textAlign: 'left'},
-                singleClickEdit:true,
-            }
         },
         mounted(){
             this.gridApi = this.gridOptions.api;
@@ -209,14 +191,3 @@
         }
     }
 </script>
-
-<!-- <ag-grid-column headerName="IT Skills">
-            <ag-grid-column field="skills" :width="120" suppressSorting
-                            cellRendererFramework="SkillsCellRenderer"
-                            :menuTabs="['filterMenuTab']">
-            </ag-grid-column>
-            <ag-grid-column field="proficiency" :width="135"
-                            cellRendererFramework="ProficiencyCellRenderer"
-                            :menuTabs="['filterMenuTab']">
-            </ag-grid-column>
-        </ag-grid-column> -->
